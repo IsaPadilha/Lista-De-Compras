ebb24919735e17ec936b667c048c1afcd72ea41c
Isadora Padilha <isap4619@gmail.com>
2026-07-08 20:37:24 -0300

    adiciona tratamento de exceção ao carregar ContextoJson

--- ListaDeCompras.ConsoleApp/Compartilhado/TelaPrincipal.cs
+++ ListaDeCompras.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -10,12 +10,8 @@ public class TelaPrincipal
     private readonly RepositorioCategoria repositorioCategoria;
     private readonly RepositorioProduto repositorioProduto;
     private readonly RepositorioListaCompras repositorioListaCompras;
-    public TelaPrincipal()
+    public TelaPrincipal(ContextoJson contexto)
     {
-        ContextoJson contexto = new ContextoJson();
-
-        contexto.Carregar();
-
         repositorioCategoria = new RepositorioCategoria(contexto);
         repositorioProduto = new RepositorioProduto(contexto);
         repositorioListaCompras = new RepositorioListaCompras(contexto);
